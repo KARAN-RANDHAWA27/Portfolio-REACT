8325bdf0740490c71e9d21ef0cf43bf262a843f1
Karandeep Randhawa <karandeeprandhawa27@gmail.com>
2024-01-01 00:26:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import 'devextreme/dist/css/dx.light.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Intro from './Components/Intro/Intro';
